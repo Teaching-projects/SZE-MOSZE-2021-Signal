8922f5ccf4d669fce526e87fb9cb21bf86aacbf4
Boci0123 <biczogabor1@gmail.com>
2021-05-08 16:08:02 +0200

    add words test

---
+++ test/wordTest.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include "../Words.h"
+#include <iostream>
+#include <map>
+#include <string>
+
+TEST(Words, getWordTest) {
+
+	std::string example = "apple";
+
+	Words p = Words("testszo.txt");
+
+	ASSERT_EQ(example, p.getWord());
+};
+TEST(Words, getTextFromFileTest) {
+
+	std::vector<std::string> example = { "apple" };
+
+	Words p = Words("testszo.txt");
+
+	std::vector<std::string> res = std::strp.getTextFromFile("testszo.txt");
+
+	ASSERT_EQ(example, res);
+};
+TEST(Words, generateWordsTest) {
+
+	std::string example = "apple";
+
+	Words p = Words("testszo.txt");
+
+	ASSERT_EQ(example, p.generateWords());
+};
+
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
