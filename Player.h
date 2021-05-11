0107c8d080843f83bc0ba5ad5785e0b93ddaf8e6
adamori97@gmail.com <adamori97@gmail.com>
2021-05-11 10:45:57 +0200

    add doxygen

--- Player.h
+++ Player.h
@@ -1,15 +1,43 @@
+/**
+ * \class Game
+ *
+ * \brief Game class
+ *
+ * This is the Game class, it contains players
+ *
+ * \version 1.0
+ *
+ * \author Signal team
+ *
+ * \date 2021/05/08 9:05
+ *
+ * Created on: 2021/05/08 9:05
+*/
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <string>
 #include <vector>
 #include <iostream>
 class Player {
-	int Point;
-	const std::string Name;
+	int Point; ///< It contains points of player
+	const std::string Name; ///< It contains name of player
 
 public:
+	/** \brief Player constructor
+      * \param name
+	*/
 	Player(const std::string name);
+
+	/** \brief Get player name
+	  * \param
+	  * \return return string
+	*/
 	const std::string getName() const;
+
+	/** \brief return point of player
+	  * \param 
+	  * \return return point
+	*/
 	int getPoint() const;
 };
 
